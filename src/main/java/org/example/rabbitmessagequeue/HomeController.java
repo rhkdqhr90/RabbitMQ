78e982af659ff08db5cddaa176f734a00c59a7a2
Gwangkyo <rhkdqhr08@gmail.com>
2025-08-28 17:09:01 +0900

    feat: RabbitMQ 생성

---
+++ src/main/java/org/example/rabbitmessagequeue/HomeController.java
@@ -0,0 +1,13 @@
+package org.example.rabbitmessagequeue;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("hello")
+    public String home(@RequestParam String p) {
+        return  "hello" + p;
+    }
+}
---
+++ src/main/java/org/example/rabbitmessagequeue/RabbitMessageQueueApplication.java
@@ -0,0 +1,13 @@
+package org.example.rabbitmessagequeue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMessageQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMessageQueueApplication.class, args);
+    }
+
+}
