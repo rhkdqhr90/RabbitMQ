b46572f3de608cbf7c1ba398fe584bddee4d9642
Gwangkyo <rhkdqhr08@gmail.com>
2025-09-17 11:15:18 +0900

    feat: RabbitMQ DeadLetterQueue

---
+++ src/main/java/org/example/rabbitmessagequeue/step7/OrderDLQConsumer.java
@@ -0,0 +1,27 @@
+package org.example.rabbitmessagequeue.step7;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderDLQConsumer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public OrderDLQConsumer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.DLQ)
+    public void process(String message) {
+        System.out.println("DLQ Message Received : " + message);
+        try{
+            String FixMessage = "success";
+            rabbitTemplate.convertAndSend(RabbitMQConfig.ORDER_EXCHANGE, "order.completed.shipping", FixMessage);
+            System.out.println("DLQ Message sent : " + FixMessage);
+        }catch (Exception e){
+            System.err.println("###[DLQ Consumer Error]" + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/org/example/rabbitmessagequeue/step7/RabbitMQConfig.java
@@ -0,0 +1,49 @@
+package org.example.rabbitmessagequeue.step7;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String ORDER_COMPLETED_QUEUE = "order_completed_queue";
+    public static final String ORDER_EXCHANGE = "order_completed_exchange";
+    public static final String DLQ = " deadLetterQueue";
+    public static final String DLX = "deadLetterExchange";
+
+    @Bean
+    public TopicExchange orderExchange() {
+        return new TopicExchange(ORDER_EXCHANGE);
+    }
+
+    @Bean
+    public TopicExchange deadLetterExchange() {
+        return new TopicExchange(DLX);
+    }
+
+    //메세지가 처리 되지못했을 경우 자동으로 Deadletterequeue이동시킴
+    @Bean
+    public Queue orderQueue() {
+        return QueueBuilder.durable(ORDER_COMPLETED_QUEUE)
+                .withArgument("x-dead-letter-exchange", DLX)
+                .withArgument("x-dead-letter-routing-key", DLQ)
+                .ttl(5000)
+                .build();
+    }
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return new Queue(DLQ);
+    }
+
+    @Bean
+    public Binding orderCompletedBinding() {
+        return BindingBuilder.bind(orderQueue()).to(orderExchange()).with("order.completed.#");
+    }
+
+    @Bean
+    public Binding deadLetterBinding() {
+        return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange()).with(DLQ);
+    }
+
+}
