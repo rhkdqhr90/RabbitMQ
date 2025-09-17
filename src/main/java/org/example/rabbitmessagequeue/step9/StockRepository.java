88476c6625bc74dd93cfbe60cd9ad33c4c61b013
Gwangkyo <rhkdqhr08@gmail.com>
2025-09-17 17:50:10 +0900

    feat: 트랜잭션 처리

---
+++ src/main/java/org/example/rabbitmessagequeue/step9/StockRepository.java
@@ -0,0 +1,9 @@
+package org.example.rabbitmessagequeue.step9;
+
+import jakarta.persistence.Entity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository<StockEntity, Long> {
+}
